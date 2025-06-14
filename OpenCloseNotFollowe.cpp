d0bc0f8619269bc117fa441656a5c28717906a24
suryaprakashverma1 <47498376+suryaprakashverma1@users.noreply.github.com>
2025-06-14 19:34:33 +0530

    Update OpenCloseNotFollowe.cpp

--- OpenCloseNotFollowe.cpp
+++ OpenCloseNotFollowe.cpp
@@ -1,15 +1,44 @@
-class FiringSystem {
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum FiringType {
+    Infrared,
+    Laser,
+    GPS
+};
+
+// Missile is now tightly coupled to firing type logic
+class Missile {
+    FiringType type;
+
 public:
-    string fire(string type) {
-        if (type == "Infrared") {
-            return "Fired using Infrared";
+    Missile(FiringType t) : type(t) {}
+
+    void execute() {
+        if (type == Infrared) {
+            cout << "Fired using Infrared guidance" << endl;
         }
-        else if (type == "Laser") {
-            return "Fired using Laser";
+        else if (type == Laser) {
+            cout << "Fired using Laser guidance" << endl;
         }
-        else if (type == "GPS") {
-            return "Fired using GPS";
+        else if (type == GPS) {
+            cout << "Fired using GPS guidance" << endl;
         }
-        return "Unknown firing type";
+        else {
+            cout << "Unknown firing type" << endl;
+        }
     }
 };
+
+int main() {
+    Missile m1(Infrared);
+    Missile m2(Laser);
+    Missile m3(GPS);
+
+    m1.execute();
+    m2.execute();
+    m3.execute();
+
+    return 0;
+}
